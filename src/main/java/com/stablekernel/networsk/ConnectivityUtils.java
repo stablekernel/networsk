02a67bfd891a07bc04eebc074dd5fe84201f486a
David Mays <david.mays@stablekernel.com>
2017-05-24 08:56:15 -0400

    consumes output streams generated by ping process

--- src/main/java/com/stablekernel/networsk/ConnectivityUtils.java
+++ src/main/java/com/stablekernel/networsk/ConnectivityUtils.java
@@ -2,7 +2,10 @@ package com.stablekernel.networsk;
 
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.concurrent.Callable;
 
 import rx.Observable;
@@ -30,7 +33,11 @@ public final class ConnectivityUtils {
         if (ipAddress != null && ipAddress.trim().length() > 0) {
             Runtime runtime = Runtime.getRuntime();
             try {
-                Process ping = runtime.exec("/system/bin/ping -c 1 " + ipAddress);
+                Process ping = runtime.exec("/system/bin/ping -q -c 1 -W 1 " + ipAddress);
+                StreamGobbler outputGobbler = new StreamGobbler(ping.getInputStream());
+                StreamGobbler errorGobbler = new StreamGobbler(ping.getErrorStream());
+                outputGobbler.start();
+                errorGobbler.start();
                 int exitValue = ping.waitFor();
                 if (Networsk.isDebug()) Log.d(Networsk.TAG, "PING exitValue: " + exitValue);
                 result = exitValue == 0;
@@ -59,5 +66,27 @@ public final class ConnectivityUtils {
                         return Observable.just(false);
                     }
                 });
+    }
+
+    static class StreamGobbler extends Thread {
+        InputStream is;
+
+        public StreamGobbler(InputStream is) {
+            this.is = is;
+        }
+
+        @Override
+        public void run() {
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line = null;
+            try {
+                while ((line = br.readLine()) != null) {
+                    if (Networsk.isDebug()) Log.v(Networsk.TAG, "GOBBLED: " + line);
+                }
+            } catch (IOException e) {
+                if (Networsk.isDebug()) Log.e(Networsk.TAG, "error while gobbling stream", e);
+            }
+        }
     }
 }
